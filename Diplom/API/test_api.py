9d8a2ad9a7c8e5af640693a436354c2725d356dd
Valentin8998 <valant991@gmail.com>
2026-07-26 09:34:39 +0300

    V1.0

--- Diplom/API/test_api.py
+++ Diplom/API/test_api.py
@@ -1,32 +1,44 @@
 import requests
 from config import key, steamId, steamIds
+import pytest
+import allure
 baseUrl = 'https://api.steampowered.com/'
 
 
-def test_inf_user():
+@pytest.mark.api
+@allure.title("информация о пользователе")
+def test_inf_user() -> None:
     url = 'ISteamUser/GetPlayerSummaries/v2/?'
     resp = requests.get(baseUrl+url+key+steamIds)
     assert resp.status_code == 200
 
 
-def test_list_game():
+@pytest.mark.api
+@allure.title("список игр")
+def test_list_game() -> None:
     url = 'IPlayerService/GetOwnedGames/v1/?'
     resp = requests.get(baseUrl+url+key+steamId)
     assert resp.status_code == 200
 
 
-def test_game_news():
+@pytest.mark.api
+@allure.title("игровые новости")
+def test_game_news() -> None:
     url = 'ISteamNews/GetNewsForApp/v2/?'
     appid = "appid= 730"
     resp = requests.get(baseUrl+url+appid)
     assert resp.status_code == 200
 
 
-def test_empty_value():
+@pytest.mark.api
+@allure.title("не валидный key")
+def test_empty_value() -> None:
     resp = requests.get(baseUrl+'IPlayerService/GetOwnedGames/v1/?key=')
     assert resp.status_code == 401
 
 
-def test_invalid_appid():
+@pytest.mark.api
+@allure.title("не валидный appid")
+def test_invalid_appid() -> None:
     resp = requests.get(baseUrl+'ISteamNews/GetNewsForApp/v2/?appid=000')
     assert resp.status_code == 403
--- Diplom/UI/news_center.py
+++ Diplom/UI/news_center.py
@@ -14,24 +14,25 @@ class NewsCenter:
         self.wait = WebDriverWait(self.driver, 10)  # Инициализация wait
         self._navigate_to_news()
 
-    def _navigate_to_news(self):
+    def _navigate_to_news(self) -> None:
         """метод для перехода на страницу новостей"""
         self.driver.get(self.BASE_URL)
         self.wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, ".DialogInput"))
             )
 
-    def game_news(self):
+    def game_news(self, game_name: str) -> None:
+        # Path of Exile 2
         """Тест поиска новостей по названию игры"""
         selector = (
             '.DialogInput.DialogInputPlaceholder.DialogTextInputBase.Focusable'
         )
         search = self.driver.find_element(By.CSS_SELECTOR, selector)
         search.click()
-        search.send_keys('Path of Exile 2')
+        search.send_keys(game_name)
         fin = self.wait.until(
          EC.element_to_be_clickable(
-            (By.XPATH, "//div[contains(text(), 'Path of Exile 2')]")
+            (By.XPATH, f"//div[contains(text(), '{game_name}')]")
             )
         )
         fin.click()
